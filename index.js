28fd2c4d047cc966ffb9e513b4f8b2252fbbdb83
Eduardo Heyer <eduardo.heyer12@gmail.com>
2023-04-17 14:13:28 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const element = document.querySelector('#main');
+element.remove();
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Eduardo Heyer is the champion";
+
+document.body.append(newHeader);
+
